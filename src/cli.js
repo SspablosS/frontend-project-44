59a199c8498a41822c87b62a380fea335d31ca81
SspablosS <pavelmeshkov2004@gmail.com>
2024-02-06 21:21:35 +0300

    new logic

--- src/cli.js
+++ src/cli.js
@@ -5,10 +5,38 @@ const greeting = (name) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
 };
 
-const randNum = () => Math.floor(Math.random() * 100);
+const randNum = (maxNum) => Math.floor(Math.random() * maxNum);
 
 const askName = () => readlineSync.question('May I have your name?: ');
+
+const checkAnswer = (answer, correctAnswer) => {
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+
+  console.log(
+    `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+  );
+
+  return false;
+};
+
+const startGame = (round) => {
+  const name = askName();
+  greeting(name);
+
+  for (let i = 0; i < 3; i += 1) {
+    if (!round(name)) {
+      return `Let's try again, ${name}`;
+    }
+  }
+
+  return `Congradulations, ${name}`;
+};
 
 export { greeting };
 export { randNum };
 export { askName };
+export { checkAnswer };
+export { startGame };
