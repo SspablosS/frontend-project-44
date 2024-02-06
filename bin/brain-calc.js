cac70361b28aeb817ac64b41681d0a6569357929
SspablosS <pavelmeshkov2004@gmail.com>
2024-02-06 21:38:42 +0300

    refactoring

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,43 +1,23 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-
-const randNum = () => Math.floor(Math.random() * 20);
-
-const addition = (a, b) => a + b;
-
-const subtraction = (a, b) => a - b;
-
-const multiplication = (a, b) => a * b;
+import { randNum, checkAnswer, startGame } from '../src/cli.js';
 
 const operators = ['+', '-', '*'];
 
-const calcGame = () => {
-  const name = readlineSync.question('May I have your name?: ');
+const playRound = () => {
+  const num1 = randNum(20);
+  const num2 = randNum(20);
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  console.log(`Question: ${num1} ${operator} ${num2}`);
+  const answer = readlineSync.questionInt('Your answer: ');
 
-  console.log(`Hello ${name}!`);
-  console.log('What is the result of the expression?');
+  let correctAnswer;
+  if (operator === '+') correctAnswer = num1 + num2;
+  if (operator === '-') correctAnswer = num1 - num2;
+  if (operator === '*') correctAnswer = num1 * num2;
 
-  for (let i = 0; i < 3; i += 1) {
-    let verification = 0;
-    const operator = operators[Math.floor(Math.random() * operators.length)];
-    const num1 = randNum();
-    const num2 = randNum();
-
-    console.log(`Question: ${num1} ${operator} ${num2}`);
-    const answer = readlineSync.questionInt('Your answer: ');
-
-    if (operator === '+') verification = addition(num1, num2);
-    if (operator === '-') verification = subtraction(num1, num2);
-    if (operator === '*') verification = multiplication(num1, num2);
-
-    if (answer === verification) {
-      console.log('Correct!');
-    } else {
-      return `'${answer}' is wrong answer ;(.Correct answer was '${verification}'.\nLet's try again, ${name}!`;
-    }
-  }
-  return `Congratulations, ${name}`;
+  return checkAnswer(answer, correctAnswer);
 };
 
-console.log(calcGame());
+startGame(playRound);
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { greeting, randNum, askName } from '../src/cli.js';
+import { randNum, checkAnswer, startGame } from '../src/cli.js';
 
 const playRound = () => {
-  const num = randNum();
+  const num = randNum(100);
   console.log(`Question: ${num}`);
   const answer = readlineSync.question('Your answer: ');
 
@@ -15,29 +15,7 @@ const playRound = () => {
     correctAnswer = 'no';
   }
 
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-
-  console.log(
-    `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
-  );
-
-  return false;
+  return checkAnswer(answer, correctAnswer);
 };
 
-const evenGame = () => {
-  const name = askName();
-  greeting(name);
-
-  for (let i = 0; i < 3; i += 1) {
-    if (!playRound(name)) {
-      return `Let's try again, ${name}`;
-    }
-  }
-
-  return `Congradulations, ${name}`;
-};
-
-console.log(evenGame());
+startGame(playRound);
