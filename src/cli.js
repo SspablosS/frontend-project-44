70e95cb0a1f9b9cbbeba841078423819d1ba3734
SspablosS <pavelmeshkov2004@gmail.com>
2024-02-06 20:59:12 +0300

    games logic added

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,14 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  const name = readlineSync.question('May I have your name?: ');
-  return `Hello ${name}!`;
+const greeting = (name) => {
+  console.log(`Hello ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
 };
 
+const randNum = () => Math.floor(Math.random() * 100);
+
+const askName = () => readlineSync.question('May I have your name?: ');
+
 export { greeting };
+export { randNum };
+export { askName };
